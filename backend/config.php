d33179cd54aee3906bce83de33d3a01d5b19ae02
omkar agawane <omkar@teafweb.com>
2023-06-30 15:01:04 +0530

    empoloyee form

--- backend/config.php
+++ backend/config.php
@@ -1,15 +1,15 @@
 <?php
-/*define("WEBSITE", "https://teafweb.com");
+define("WEBSITE", "https://teafweb.com");
 define("SERVER", "localhost");
 define("USERNAME", "u702940361_teafwebU");
 define("PASSWORD", "+aR8;Z>8F7S");
-define("DATABASE", "u702940361_teafweb");*/
-
+define("DATABASE", "u702940361_teafweb");
+/*
 define("WEBSITE", "localhost/teafweb.in/");
 define("SERVER", "localhost");
 define("USERNAME", "root");
 define("PASSWORD", "");
-define("DATABASE", "teafweb");
+define("DATABASE", "teafweb");*/
 
 
 ?>
