d2649846af912ad4ea284ce8e8cca90f74b689cc
rituja <rituja@teafweb.com>
2023-11-06 20:38:49 +0530

    career linkedin

--- career.php
+++ career.php
@@ -30,10 +30,12 @@
                 <div>
                 <h3 class="h5 m1">It could be the beginning of a more potent version of yourself.</h3><br>
                 
+                <div class="m1"></div>
                 <p class="p">Follow Teafweb on linkedin for update and opportunity</p>
                 <script type="IN/FollowCompany" data-id="76340003" data-counter="bottom"></script>
                 <br>
                 <br>
+                </div>
 
                     <form id="mySignFormP2" name="signin" enctype="multipart/form-data"
                         onsubmit="event.preventDefault();signinFormPrevent('mySignFormP2')"
